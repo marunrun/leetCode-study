ace9cc4cfb339e606acc8a816bb780ab25bef52c
marun <marunrun@163.com>
2021-02-24 10:58:34 +0800

    feat  : 2021-02-24 打卡 😀

---
+++ algorithm/src/main/java/leetCode/LeetCode832.java
@@ -0,0 +1,74 @@
+package main.java.leetCode;
+
+import java.util.Arrays;
+
+public class LeetCode832 {
+
+    /**
+     * 给定一个二进制矩阵 A，我们想先水平翻转图像，然后反转图像并返回结果。
+     * <p>
+     * 水平翻转图片就是将图片的每一行都进行翻转，即逆序。例如，水平翻转 [1, 1, 0] 的结果是 [0, 1, 1]。
+     * <p>
+     * 反转图片的意思是图片中的 0 全部被 1 替换， 1 全部被 0 替换。例如，反转 [0, 1, 1] 的结果是 [1, 0, 0]。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：[[1,1,0],[1,0,1],[0,0,0]]
+     * 输出：[[1,0,0],[0,1,0],[1,1,1]]
+     * 解释：首先翻转每一行: [[0,1,1],[1,0,1],[0,0,0]]；
+     * 然后反转图片: [[1,0,0],[0,1,0],[1,1,1]]
+     * 示例 2：
+     * <p>
+     * 输入：[[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+     * 输出：[[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+     * 解释：首先翻转每一行: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]]；
+     * 然后反转图片: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/flipping-an-image
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static int[][] flipAndInvertImage2(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                if (A[i][left] == A[i][right]) {
+                    A[i][left] ^= 1;
+                    A[i][right] ^= 1;
+                }
+                left++;
+                right--;
+            }
+            if (left == right) {
+                A[i][left] ^= 1;
+            }
+        }
+        return A;
+    }
+    public static int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            int len = A[i].length -1;
+            int m = (len >> 1 ) + 1;
+            for (int j = 0; j < m; j++) {
+                int tmp = A[i][j] ^ 1;
+                A[i][j] = A[i][len - j] ^ 1;
+                A[i][len - j] = tmp;
+            }
+        }
+
+        return A;
+    }
+
+    public static void main(String[] args) {
+        int[][] ints = flipAndInvertImage(new int[][]{
+                {1, 1, 0},
+                {1, 0, 1},
+                {0, 0, 0},
+        });
+        System.out.println(Arrays.deepToString(ints));
+    }
+}
