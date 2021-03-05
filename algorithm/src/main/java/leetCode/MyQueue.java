c9a3ed67395b5699974052d5300744ff114bbd61
marun <marunrun@163.com>
2021-03-05 14:21:02 +0800

    feat  : 2021-03-05 打卡

--- algorithm/src/main/java/leetCode/MyQueue.java
+++ algorithm/src/main/java/leetCode/MyQueue.java
@@ -2,47 +2,58 @@ package main.java.leetCode;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Stack;
 
 class MyQueue {
 
-    Deque<Integer> inQueue;
-    Deque<Integer> outQueue;
+    Stack<Integer> inQueue;
+    Stack<Integer> outQueue;
 
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public MyQueue() {
-        this.inQueue = new LinkedList<Integer>();
-        this.outQueue = new LinkedList<Integer>();
+        this.inQueue = new Stack<>();
+        this.outQueue = new Stack<>();
     }
 
-    /** Push element x to the back of queue. */
+    /**
+     * Push element x to the back of queue.
+     */
     public void push(int x) {
         inQueue.push(x);
     }
 
-    /** Removes the element from in front of queue and returns that element. */
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
     public int pop() {
-        resetOutQueue();
+        if (outQueue.isEmpty()) {
+            resetOutQueue();
+        }
         return outQueue.pop();
     }
 
-    /** Get the front element. */
+    /**
+     * Get the front element.
+     */
     public int peek() {
-        resetOutQueue();
+        if (outQueue.isEmpty()) {
+            resetOutQueue();
+        }
         return outQueue.peek();
     }
 
-    private void resetOutQueue()
-    {
-        if (!inQueue.isEmpty() && outQueue.isEmpty()) {
-            int size = inQueue.size();
-            for (int i = 0; i < size; i++) {
-                outQueue.push(inQueue.pop());
-            }
+    private void resetOutQueue() {
+        while (!inQueue.isEmpty()) {
+            outQueue.push(inQueue.pop());
         }
     }
 
-    /** Returns whether the queue is empty. */
+    /**
+     * Returns whether the queue is empty.
+     */
     public boolean empty() {
         return inQueue.isEmpty() && outQueue.isEmpty();
     }
