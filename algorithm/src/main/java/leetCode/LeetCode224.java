6f14e235635de1484c5e2cab7f7f53fbb8224d53
marun <marunrun@163.com>
2021-03-10 15:59:25 +0800

    feat  : 2021-03-09 打卡 复习

--- algorithm/src/main/java/leetCode/LeetCode224.java
+++ algorithm/src/main/java/leetCode/LeetCode224.java
@@ -6,11 +6,54 @@ import java.util.Stack;
 public class LeetCode224 {
     public static void main(String[] args) {
         int i = 3 - (5 - (8) - (2 + (9 - (0 - (8 - (2)))) - (4)) - (4));
-
+        System.out.println(i);
         System.out.println(calculate("(3-(5-(8)-(2+(9-(0-(8-(2))))-(4))-(4)))"));
     }
 
+
+    // 更简易的方法
     public static int calculate(String s) {
+        // res 保存计算结果。num保存当前值。 sign保存操作符号
+        int res = 0, num = 0, sign = 1;
+        Stack<Integer> stack = new Stack<>();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                //如果是连续的数字，需要*10 即 进一位
+                // 如： 111  第一次计算为 0 * 1 +1 = 1 第二次计算为 1* 10 +1 = 11 第三次计算为 11 * 10 + 1
+                // -'0' 是为了得到真正的数字
+                num = 10 * num + c - '0';
+            } else if (c == '+' || c == '-') {
+                res += num * sign;
+
+                // 不再是连续的数字，num清空
+                num = 0;
+                if (c == '-') {
+                    sign = -1;
+                } else {
+                    sign = 1;
+                }
+            } else if (c == '(') {
+                // 遇到左括号 将左侧的计算结果和操作符保存下来 并将结果清空
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                // 遇到右括号，将当前计算完成，并将上个左括号之前的结果计算
+                res += sign * num;
+                num = 0;
+                res *= stack.pop();
+                res += stack.pop();
+            }
+        }
+        res += sign * num;
+        return res;
+    }
+
+    // 我是sb 写了一长串 🤐
+    public static int calculate2(String s) {
         if (s.length() < 2) return Integer.parseInt(s);
         Stack<Integer> numbers = new Stack<>();
         Stack<Character> chars = new Stack<>();
@@ -48,7 +91,7 @@ public class LeetCode224 {
                             Character operator = chars.peek();
                             if (operator == '(') {
                                 chars.pop();
-                            }else if (chars.peek() != '('){
+                            } else if (chars.peek() != '(') {
                                 cacl(numbers, chars);
                             }
                             break;
